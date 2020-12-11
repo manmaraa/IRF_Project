2f504b4167d7a018c4d56fd9df24436bcc9213c1
Matin Manhertz <manhertzmartin@outlook.hu>
2020-12-11 18:33:21 +0100

    folytatás

--- ManhertzMartinBeadando/ManhertzMartinBeadando/Form2.Designer.cs
+++ ManhertzMartinBeadando/ManhertzMartinBeadando/Form2.Designer.cs
@@ -28,19 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 442);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Vissza";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(832, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 101);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Küldés fájlba";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(616, 33);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 101);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(36, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(574, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ha bármi kérdése van, vagy problémát talált itt írja le:";
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(976, 523);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form2";
             this.Text = "Form2";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- ManhertzMartinBeadando/ManhertzMartinBeadando/Form2.cs
+++ ManhertzMartinBeadando/ManhertzMartinBeadando/Form2.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,39 @@ namespace ManhertzMartinBeadando
 {
     public partial class Form2 : Form
     {
+        List<string> kerdesek = new List<string>();
         public Form2()
         {
             InitializeComponent();
+            
         }
+        
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                foreach (var item in kerdesek)
+                {
+                    kerdesek.Add(textBox1.Text);
+                }
+                foreach (var item in kerdesek)
+                {
+                    sw.WriteLine(item);
+                }
+               
+            }
+        }
+        
     }
 }
